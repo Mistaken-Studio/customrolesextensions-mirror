b2041a4fc5653f1c3ab88f4e0ae7d38b2db7551e
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-11-20 15:49:19 +0100

    Some extensions + BuiltInPermissions fix

--- CustomRolesExtensions/MistakenCustomRole.cs
+++ CustomRolesExtensions/MistakenCustomRole.cs
@@ -5,7 +5,9 @@
 // -----------------------------------------------------------------------
 
 using Exiled.API.Enums;
+using Exiled.API.Features;
 using Exiled.CustomRoles.API.Features;
+using Mistaken.API.Extensions;
 
 namespace Mistaken.API.CustomRoles
 {
@@ -16,6 +18,11 @@ namespace Mistaken.API.CustomRoles
         public static MistakenCustomRole Get(MistakenCustomRoles id)
             => Get((int)id) as MistakenCustomRole;
 
+        /// <inheritdoc cref="CustomRole.Get(int)"/>
+        public static T Get<T>(MistakenCustomRoles id)
+            where T : MistakenCustomRole, new()
+            => Get((int)id) as T;
+
         /// <inheritdoc cref="CustomRole.TryGet(int, out Exiled.CustomRoles.API.Features.CustomRole)"/>
         public static bool TryGet(MistakenCustomRoles id, out MistakenCustomRole customRole)
         {
@@ -23,6 +30,17 @@ namespace Mistaken.API.CustomRoles
             if (!TryGet((int)id, out var customRole2))
                 return false;
             customRole = customRole2 as MistakenCustomRole;
+            return true;
+        }
+
+        /// <inheritdoc cref="CustomRole.TryGet(int, out Exiled.CustomRoles.API.Features.CustomRole)"/>
+        public static bool TryGet<T>(MistakenCustomRoles id, out T customRole)
+            where T : MistakenCustomRole, new()
+        {
+            customRole = null;
+            if (!TryGet((int)id, out var customRole2))
+                return false;
+            customRole = customRole2 as T;
             return true;
         }
 
@@ -39,6 +57,22 @@ namespace Mistaken.API.CustomRoles
         {
             get => (uint)this.CustomRole;
             set => _ = value;
+        }
+
+        /// <inheritdoc/>
+        protected override void RoleAdded(Player player)
+        {
+            base.RoleAdded(player);
+            if (this.BuiltInPermissions != KeycardPermissions.None)
+                player.SetSessionVariable(SessionVarType.BUILTIN_DOOR_ACCESS, this.BuiltInPermissions);
+        }
+
+        /// <inheritdoc/>
+        protected override void RoleRemoved(Player player)
+        {
+            base.RoleRemoved(player);
+            if (this.BuiltInPermissions != KeycardPermissions.None)
+                player.RemoveSessionVariable(SessionVarType.BUILTIN_DOOR_ACCESS);
         }
     }
 }
